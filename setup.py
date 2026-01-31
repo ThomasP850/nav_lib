725898422d205094fd487cde42cc347019988604
thomasp850 <tp8505583@gmail.com>
2026-01-31 15:17:27 -0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# Include all subpackages (nav_lib, nav_lib.motion, nav_lib.planner, ...)
+# Include nav_lib and all subpackages (nav_lib.planner, nav_lib.motion, nav_lib.pointcloud)
 setup_args = generate_distutils_setup(
-    packages=['nav_lib'],
+    packages=find_packages('lib'),
     package_dir={'': 'lib'},
     install_requires=['open3d'],
 )
